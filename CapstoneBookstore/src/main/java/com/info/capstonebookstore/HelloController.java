702ff7dee1b8e14a9a089bb59a9217a08f87ebf5
Ranjit Pandher <ranjit.pandher@email.kpu.ca>
2022-07-15 13:13:58 -0700

    initial setup

--- CapstoneBookstore/src/main/java/com/info/capstonebookstore/HelloController.java
+++ CapstoneBookstore/src/main/java/com/info/capstonebookstore/HelloController.java
@@ -1,14 +1,31 @@
 package com.info.capstonebookstore;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    void onHelloButtonClick(ActionEvent event) {
+
     }
+
+    @FXML
+    void initialize() {
+        assert welcomeText != null : "fx:id=\"welcomeText\" was not injected: check your FXML file 'hello-view.fxml'.";
+
+    }
+
 }
